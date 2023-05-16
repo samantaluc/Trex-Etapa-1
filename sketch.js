408aeda1d6d084eed43cae38e75eeaa29117441a
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-05-16 16:43:52 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-var trex, trex_running, trex_collided; //troca edges por trex_collided 08/04
-var ground, invisibleGround, groundImage; //adiciona ground e invisibleGround 08/04
+var trex, trex_running, trex_collided; //troca edges por trex_collided <<<<<<<
+var ground, invisibleGround, groundImage; //adiciona ground e invisibleGround <<<<
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  trex_collided = loadImage("trex_collided.png"); //carrega imagem do trex collided 08/04
+  trex_collided = loadImage("trex_collided.png"); //carrega imagem do trex collided <<<<<<
   groundImage = loadImage("ground2.png")
 }
 function setup() {
@@ -14,15 +14,15 @@ function setup() {
   trex.scale = 0.5;
   trex.x = 50 
 
-  ground = createSprite(200,180,400,20); //cria o sprite 08/04
-  ground.addImage("ground",groundImage); //adiciona imagem ao sprite 08/04
-  ground.x = ground.width /2; //cria posição do sprite 08/04
+  ground = createSprite(200,180,400,20); //cria o sprite <<<<<<<<<
+  ground.addImage("ground",groundImage); //adiciona imagem ao sprite <<<<<<
+  ground.x = ground.width /2; //cria posição do sprite <<<<<<
 }
 function draw() {
   background(220);  
-  ground.velocityX = -2; //velocidade do chão indo da direita pra esquerda   08/04
-  if (ground.x<0){ //se a posição do chão em x for menor que zero  08/04
-    ground.x = ground.width/2; //volta a posição original  08/04
+  ground.velocityX = -2; //velocidade do chão indo da direita pra esquerda <<<<<<
+  if (ground.x<0){ //se a posição do chão em x for menor que zero <<<<<<
+    ground.x = ground.width/2; //volta a posição original <<<<<<
   }  
   //pular quando a tecla espaço for pressionada
   if(keyDown("space")) {
@@ -30,6 +30,6 @@ function draw() {
   }  
   trex.velocityY = trex.velocityY + 0.8   
  //impedir que o trex caia
-  trex.collide(ground); //troca o edges[3] por ground  08/04
+  trex.collide(ground); //troca o edges[3] por ground <<<<<<
   drawSprites();
 }
